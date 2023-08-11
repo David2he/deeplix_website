2af21a46de32d5225916a773afbf00896ab6312c
David Dehe <david.dehe.pro@gmail.com>
2023-08-11 05:22:11 +0200

    test / fix deploying while next js bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+    generateBuildId: async () => {
+      // Return custom build ID, like the latest git commit hash
+      return 'my-build-id'
+    }
+  }
