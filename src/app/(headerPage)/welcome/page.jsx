e40756b4b76d040580fcaf8138f9a4e861d48f6b
David Dehe <david.dehe.pro@gmail.com>
2023-09-09 12:00:21 +0200

    fix / deploy

--- src/app/(headerPage)/welcome/page.jsx
+++ src/app/(headerPage)/welcome/page.jsx
@@ -1,17 +1,16 @@
 "use client";
-
 import { useSearchParams } from "next/navigation";
 import style from "./page.module.scss";
 import { CarrouselPicturesFirstPage } from "../../../components/CarrouselPicturesFirstPage/CarrouselPicturesFirstPage";
 import { WelcomTextFirstPage } from "../../../components/WelcomTextFirstPage/WelcomTextFirstPage";
 export default function Home() {
-  const searchParams = useSearchParams();
-  const type = searchParams.get("type");
+    const searchParams = useSearchParams();
+    const type = searchParams.get("type");
 
-  return (
-    <main className={style.mainContainer}>
-      <WelcomTextFirstPage type={type} />
-      <CarrouselPicturesFirstPage />
-    </main>
-  );
+    return (
+        <main className={style.mainContainer}>
+            <WelcomTextFirstPage />
+            <CarrouselPicturesFirstPage />
+        </main>
+    );
 }
