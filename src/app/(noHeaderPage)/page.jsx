f2bef17972e90e13a45e41c5bb70f62c4a7a22fd
David Dehe <david.dehe.pro@gmail.com>
2023-09-10 03:27:52 +0200

    fix / correctif

--- src/app/(noHeaderPage)/page.jsx
+++ src/app/(noHeaderPage)/page.jsx
@@ -1,9 +1,9 @@
 import style from "./Login.module.scss";
 import { FormLogin } from "@/components/FormLogin/FormLogin";
 export default function Home() {
-  return (
-    <div className={style.LoginPageContainer}>
-      <FormLogin />
-    </div>
-  );
+    return (
+        <div className={style.LoginPageContainer}>
+            <FormLogin />
+        </div>
+    );
 }
--- src/components/BuyDeepOne/BuyDeepOneSecondPart/BuyDeepOneSecondPart.jsx
+++ src/components/BuyDeepOne/BuyDeepOneSecondPart/BuyDeepOneSecondPart.jsx
@@ -5,10 +5,10 @@ import { useState, useEffect } from "react";
 export const BuyDeepOneSecondPart = ({ step, setStep }) => {
     const [canAppear, setCanAppear] = useState(false);
     const [finisTextGeneration, setFinisTextGeneration] = useState(false);
-    const [inBetweenFinishTextGeneration, setInBetweenFinishTextGeneration] =
-        useState(true);
+    const [inBetweenFinishTextGeneration, setInBetweenFinishTextGeneration] = useState(true);
     const [appearBlock6to7, setAppearBlock6to7] = useState(false);
     const [appearBlock7to8, setAppearBlock7to8] = useState(false);
+    const [appearBlock8to9, setAppearBlock8to9] = useState(false);
     const randomNumberContent = [
         "InnerBrownRaise",
         "LipPress",
@@ -18,18 +18,21 @@ export const BuyDeepOneSecondPart = ({ step, setStep }) => {
         "Surprise",
         "Valences",
         "Engagement",
+        "Joy",
+        "Disgust",
+        "Anger",
+        "Sadness",
+        "LipSuck",
+        "LipStretch",
+        "LipCornerDepressor",
+        "Mouthopen",
+        "LipCornerPuller",
     ];
     const renderBetween4And5 = () => {
         return (
             <>
-                <p>
-                    Place yourself in front of the camera and we'll proceed to
-                    scan your facial expressions.
-                </p>
-                <div
-                    className={style.redButton}
-                    onClick={() => navigateThrougStep("forward")}
-                >
+                <p>Place yourself in front of the camera and we'll proceed to scan your facial expressions.</p>
+                <div className={style.redButton} onClick={() => navigateThrougStep("forward")}>
                     <p>CONTINUE</p>
                 </div>
             </>
@@ -39,47 +42,19 @@ export const BuyDeepOneSecondPart = ({ step, setStep }) => {
         return (
             <div className={style.container5And6}>
                 <div className={style.scanningPContainer}>
-                    <p
-                        className={
-                            inBetweenFinishTextGeneration
-                                ? null
-                                : style.finishScanning
-                        }
-                    >
-                        {" "}
-                        Scanning
-                    </p>
-                    <div
-                        className={
-                            inBetweenFinishTextGeneration
-                                ? style.isScanningDOT
-                                : style.finishScanningDOT
-                        }
-                    >
+                    <p className={inBetweenFinishTextGeneration ? null : style.finishScanning}> Scanning</p>
+                    <div className={inBetweenFinishTextGeneration ? style.isScanningDOT : style.finishScanningDOT}>
                         <span></span>
                         <span></span>
                         <span></span>
                     </div>
                 </div>
-                <ul
-                    className={
-                        inBetweenFinishTextGeneration
-                            ? style.appearLi
-                            : style.disappearLi
-                    }
-                >
+                <ul className={inBetweenFinishTextGeneration ? style.appearLi : style.disappearLi}>
                     <li>Measurements</li>
                     <li>Expressions</li>
                     <li>Ethnicity European</li>
                     {randomNumberContent.map((content, index) => {
-                        return (
-                            <GeneratedText
-                                key={content}
-                                content={content}
-                                index={index}
-                                setFinisTextGeneration={setFinisTextGeneration}
-                            />
-                        );
+                        return <GeneratedText key={content} content={content} index={index} setFinisTextGeneration={setFinisTextGeneration} />;
                     })}
                 </ul>
             </div>
@@ -88,36 +63,18 @@ export const BuyDeepOneSecondPart = ({ step, setStep }) => {
 
     const renderBetween6And7 = () => {
         return (
-            <div className={style.container6And7}>
-                <div
-                    className={`${style.titleText} ${
-                        appearBlock6to7 ? null : style.block67CanAppear
-                    }`}
-                >
-                    <p>
-                        Please read this excerpt from Fight Club®, 1999, David
-                        Fincher out loud, articulating :
-                    </p>
+            <div className={` ${style.container6And7} ${appearBlock7to8 && style.disappearcontainer6And7}`}>
+                <div className={`${style.titleText} ${appearBlock6to7 ? null : style.block67CanAppear}`}>
+                    <p>Please read this excerpt from Fight Club®, 1999, David Fincher out loud, articulating :</p>
                 </div>
-                <div
-                    className={`${style.italicText} ${
-                        appearBlock6to7 ? null : style.block67CanAppear
-                    }`}
-                >
+                <div className={`${style.italicText} ${appearBlock6to7 ? null : style.block67CanAppear}`}>
                     <p>
-                        Warning: If you are reading this then thiswarning is for
-                        you. Every word you read of this useless fine print is
-                        another second of your life. Don't you have other things
-                        to do? Is your life so empty that you honestly can't
-                        think etter way to spend these moments? Or are you so
-                        impressed with authority that you give respect and
-                        credence to all that claim it? Do you read everything
-                        you're supposed to read? Do you think every thing you're
-                        supposed to think? Buy what you're told to want? Get out
-                        of your apartment. Meet a member of the opposite sex.
-                        Stop the excessive shopping and masturbation. Quit your
-                        job. Start a fight. Prove you're alive. If you don't
-                        claim your humanity you will become a statistic.
+                        Warning: If you are reading this then thiswarning is for you. Every word you read of this useless fine print is another second
+                        of your life. Don't you have other things to do? Is your life so empty that you honestly can't think etter way to spend these
+                        moments? Or are you so impressed with authority that you give respect and credence to all that claim it? Do you read
+                        everything you're supposed to read? Do you think every thing you're supposed to think? Buy what you're told to want? Get out
+                        of your apartment. Meet a member of the opposite sex. Stop the excessive shopping and masturbation. Quit your job. Start a
+                        fight. Prove you're alive. If you don't claim your humanity you will become a statistic.
                     </p>
                 </div>
             </div>
@@ -127,32 +84,27 @@ export const BuyDeepOneSecondPart = ({ step, setStep }) => {
     const renderBetween7And8 = () => {
         return (
             <>
-                <div
-                    className={appearBlock7to8 ? style.block67CanAppear : null}
-                >
-                    <p>
-                        Now step back a few meters and blink when you're in
-                        position to calibrate your movements.
-                    </p>
-                    <div
-                        className={style.redButton}
-                        onClick={() => navigateThrougStep("forward")}
-                    >
+                <div className={`${style.container7and8} ${appearBlock7to8 && style.appearBlock7to8}`}>
+                    <p>Now step back a few meters and blink when you're in position to calibrate your movements.</p>
+                    <div className={style.redButton} onClick={() => navigateThrougStep("forward")}>
                         <p>CONTINUE</p>
                     </div>
                 </div>
             </>
         );
     };
-
-    const onPressSpace = (event) => {
-        if (event.code === "Space") {
-            setAppearBlock7to8(true);
-            let timer = setTimeout(() => {
-                setStep(step + 1);
-                clearTimeout(timer);
-            }, 1500);
-        }
+    const renderBetween8And9 = () => {
+        return (
+            <>
+                <div className={`${style.container8and9} ${appearBlock8to9 && style.appearBlock7to8}`}>
+                    <div className={style.videoWrapper}>
+                        <video autoPlay loop className={style.fullscreenVideo}>
+                            <source src="/intro.mp4" />
+                        </video>
+                    </div>
+                </div>
+            </>
+        );
     };
 
     const navigateThrougStep = (direction) => {
@@ -165,11 +117,7 @@ export const BuyDeepOneSecondPart = ({ step, setStep }) => {
     };
 
     useEffect(() => {
-        let timerForStep4,
-            timerForStep5_1,
-            timerForStep5_2,
-            timerForStep5_3,
-            timerForStep6_1;
+        let timerForStep4, timerForStep5_1, timerForStep5_2, timerForStep5_3, timerForStep6_1, timerForStep7_1, timerForStep7_2;
 
         if (step === 4 && canAppear === false) {
             timerForStep4 = setTimeout(() => {
@@ -192,28 +140,39 @@ export const BuyDeepOneSecondPart = ({ step, setStep }) => {
             timerForStep6_1 = setTimeout(() => {
                 setAppearBlock6to7(true);
             }, 1500);
-            window.addEventListener("keydown", onPressSpace);
+            timerForStep7_1 = setTimeout(() => {
+                setAppearBlock7to8(true);
+                timerForStep7_2 = setTimeout(() => {
+                    navigateThrougStep("forwards");
+                }, 1000);
+            }, 5000);
+
+            if (step === 7) {
+                setAppearBlock8to9(true);
+            }
         }
 
         return () => {
             clearTimeout(timerForStep4);
             clearTimeout(timerForStep5_1, timerForStep5_2, timerForStep5_3);
             clearTimeout(timerForStep6_1);
+            clearTimeout(timerForStep7_1);
+            clearTimeout(timerForStep7_2);
         };
     }, [canAppear, step, finisTextGeneration]);
 
     return (
         <div className={style.container}>
-            <div
-                className={`${canAppear ? style.canAppear : style.stayHidden}`}
-            >
+            <div className={`${canAppear ? style.canAppear : style.stayHidden}`}>
                 {step === 4
                     ? renderBetween4And5()
                     : step === 5
                     ? renderBetween5And6()
                     : step === 6
                     ? renderBetween6And7()
-                    : renderBetween7And8()}
+                    : step === 7
+                    ? renderBetween7And8()
+                    : renderBetween8And9()}
             </div>
         </div>
     );
