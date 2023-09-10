671313eb76932b294e65445fe48aeb3e8f17947e
David Dehe <david.dehe.pro@gmail.com>
2023-09-10 03:40:38 +0200

    add  / video intro

--- src/app/(noHeaderPage)/page.jsx
+++ src/app/(noHeaderPage)/page.jsx
@@ -1,9 +1,23 @@
+"use client";
+import { useState } from "react";
 import style from "./Login.module.scss";
 import { FormLogin } from "@/components/FormLogin/FormLogin";
+
 export default function Home() {
+    const [videoEnded, setVideoEnded] = useState(false);
+
     return (
-        <div className={style.LoginPageContainer}>
-            <FormLogin />
+        <div className={style.container}>
+            <div className={style.videoIntroContainer}>
+                <video autoPlay className={style.fullscreenVideo} onEnded={() => setVideoEnded(true)}>
+                    <source src="./DEEPFLIX_INTROWEBSITE.mp4" />
+                </video>
+            </div>
+            {videoEnded && (
+                <div className={style.LoginPageContainer}>
+                    <FormLogin />
+                </div>
+            )}
         </div>
     );
 }
