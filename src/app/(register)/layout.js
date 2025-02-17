02edd15c7c8c0ad26944db5624dbb994dcc13100
david <david.dehe.pro@gmail.com>
2025-02-17 15:08:42 +0100

    fix responsive

--- src/app/(register)/layout.js
+++ src/app/(register)/layout.js
@@ -9,6 +9,7 @@ export default function NoHeaderPage({ children }) {
 	return (
 		<html lang="fr">
 			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 				<link rel="icon" href="/favicon.ico" />
 			</head>
 			<body>{children}</body>
